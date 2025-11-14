19a86348f05f34fc77560a83795bc0a01a76513f
sam-kash <kashyapsankalpa@gmail.com>
2025-11-15 00:02:45 +0530

    added schemas

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-
+from app.schemas import PostCreate  
 app = FastAPI()
 
 text_posts = {1: {"title":"New Post", "content":"cool test post"} }
@@ -12,18 +12,25 @@ def hello_world():
 def first():
     return {"message" : "write the url as piutube to actually start"}
 
-@app.get("/piutube")
+@app.get("/piutube")    # From here you look at the actual code     
 def piutube():
     return {"message" : "welcome to piutube"} 
 
 @app.get("/piutube/posts")
 def get_all_posts(limit: int =10):
     if limit:
-        return text_posts[:limit]
+        return list(text_posts.vaues())[:limit]
     return text_posts
 
 @app.get("/piutube/posts/{id}") 
 def get_post(id: int):
     if id not in text_posts:
-        raise HTTPException(status_code=404, detaiil ="Post not found")
+        raise HTTPException(status_code=404, detail ="Post not found")
+    
     return text_posts.get(id)
+
+@app.post("/piutube/posts")
+def create_post(post: PostCreate):  
+    new_post = {"title" : post.title , "content" : post.content}
+    text_posts[max(text_posts.keys())+1] = new_post
+    return new_post
