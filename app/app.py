54a4ac1efe95aa99ca4ca3b06bc0df4d307411df
sam-kash <kashyapsankalpa@gmail.com>
2025-11-14 07:25:46 +0530

    limit

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
+text_posts = {1: {"title":"New Post", "content":"cool test post"} }
 
 @app.get("/hello-world")
 def hello_world():
@@ -8,8 +10,20 @@ def hello_world():
 
 @app.get("/")
 def first():
-    return {"message" : "write the url as piutube"}
+    return {"message" : "write the url as piutube to actually start"}
 
 @app.get("/piutube")
 def piutube():
-    return {"message" : "welcome to piutube"}
+    return {"message" : "welcome to piutube"} 
+
+@app.get("/piutube/posts")
+def get_all_posts(limit: int =10):
+    if limit:
+        return text_posts[:limit]
+    return text_posts
+
+@app.get("/piutube/posts/{id}") 
+def get_post(id: int):
+    if id not in text_posts:
+        raise HTTPException(status_code=404, detaiil ="Post not found")
+    return text_posts.get(id)
