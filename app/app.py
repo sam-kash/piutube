6bf9495783e3199131848300328b0629024d6c90
sam-kash <kashyapsankalpa@gmail.com>
2025-11-18 19:37:36 +0530

    modified end point with upload as post

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 from app.schemas import PostCreate  
 from app.schemas import PostResponse
 from app.db import Post, create_db_and_tables, get_async_session
@@ -13,37 +13,15 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-text_posts = {1: {"title":"New Post", "content":"cool test post"} }
-
-@app.get("/hello-world")
-def hello_world():
-    return {"message" : "Hello-world"}
-
-@app.get("/")
-def first():
-    return {"message" : "write the url as piutube to actually start"}
-
-@app.get("/piutube")    # From here you look at the actual code     
-def piutube():
-    return {"message" : "welcome to piutube"} 
-
-@app.get("/piutube/posts")
-def get_all_posts(limit: int =10):
-    if limit:
-        return list(text_posts.values())[:limit]
-    return text_posts
-
-@app.get("/piutube/posts/{id}") 
-def get_post(id: int) -> PostResponse:
-    if id not in text_posts:
-        raise HTTPException(status_code=404, detail ="Post not found")
-    
-    return text_posts.get(id)
-
-@app.post("/piutube/posts")
-def create_post(post: PostCreate) -> PostResponse:  
-    new_post = {"title" : post.title , "content" : post.content}
-    text_posts[max(text_posts.keys())+1] = new_post
-    return new_post
-    
-
+@app.post("/upload")
+async def upload_file(
+    file: UploadFile = File(...),
+    caption: str = Form(""),
+    session: AsyncSession = Depends(get_async_session)
+):
+    post = Post(
+        caption = caption,
+        url = "dummy url",
+        file_type = "photo",
+        file_name="dummy name"
+    )
